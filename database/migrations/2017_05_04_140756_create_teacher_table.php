992123b31086d4595a481a1f3824e17c0b9f4baa
Unknown <waritthorn.v@ku.th>
2017-05-04 21:51:53 +0700

    table has been added

---
+++ database/migrations/2017_05_04_140756_create_teacher_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTeacherTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::create('teacher', function (Blueprint $table) {
+          $table->increments('no');
+          $table->string('path');
+          $table->string('fname');
+          $table->string('lname');
+          $table->string('description');
+          $table->rememberToken();
+          $table->timestamps();
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
