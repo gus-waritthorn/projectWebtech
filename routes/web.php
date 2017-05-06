9bb9c19bd3c3b4263bff517433ffa83c2c65fcce
Kamonwut <kamonwut.c@ku.th>
2017-05-06 16:11:38 +0700

    add role admin, student in adminpage but not add redirect

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', ['middleware' => ['admin', 'auth'], function(){
+    return view('home');
+}]);
