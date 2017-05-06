46dd3861b5def8d470c0769d1632d2db589e6936
dizzenter <sirasith.k@ku.th>
2017-05-06 18:24:05 +0700

    fix responsive / path img

--- resources/views/layouts/_navbar.blade.php
+++ resources/views/layouts/_navbar.blade.php
@@ -1,35 +1,46 @@
-<nav class="navbar navbar-default navbar-fixed-top">
-		    		<div class="navbar-header">
-							<!-- Collapsed Hamburger -->
-								<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-										<span class="sr-only">Toggle Navigation</span>
-										<span class="icon-bar"></span>
-										<span class="icon-bar"></span>
-										<span class="icon-bar"></span>
-								</button>
+<nav class="navbar navbar-default navbar-static-top navbar-fixed-top" style="">
+            <div class="container">
+                <div class="navbar-header">
 
-		      			<a class="navbar-brand" href="{{ url('/') }}">
-		        			<img alt="Brand" id="home-icon" src="http://www.novelsol.com/wp-content/uploads/2014/12/flaticon-grad.png">
-									<div class=" navbar-nav  mslogo">
-		                <a href="{{ url('/') }}" style="font-family: 'Acme', sans-serif;font-size: 25px;"> Music School</a>
-		              </div>
-		      			</a>
-		      		</div>
-							  <div class="collapse navbar-collapse" id="app-navbar-collapse">
-		      <ul class="nav navbar-nav navbar-right top-right">
-         @if (Route::has('login'))
-             @if (Auth::check())
-                 <li><a href=""> My Course</a></li>
-                 <li class="divider-vertical"></li>
-                 <li class="dropdown">
+                    <!-- Collapsed Hamburger -->
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                        <span class="sr-only">Toggle Navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+
+                    <!-- Branding Image -->
+                    <div class="navbar-header">
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        <img alt="Brand" id="home-icon" src="http://www.novelsol.com/wp-content/uploads/2014/12/flaticon-grad.png">
+                    </a>
+                    </div>
+                </div>
+
+                <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="nav navbar-nav">
+                    <li>                <a href="{{ url('/') }}" style="font-family: 'Acme', sans-serif;font-size: 25px;color: black;margin-left: 3rem;"> Music School</a></li>
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="nav navbar-nav navbar-right">
+                        <!-- Authentication Links -->
+                        @if (Auth::guest())
+                            <li><a href="{{ route('login') }}">Login</a></li>
+                            <li><a href="{{ route('register') }}">Register</a></li>
+                        @else
+                            <li><a href=""> My Course</a></li>                            
+                            <li class="divider-vertical"></li>
+                            <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->email }} <span class="caret"></span>
                                 </a>
 
                                 <ul class="dropdown-menu" role="menu">
-                                	<li><a href="">My Profile</a></li>
-                                	<li><a href="">My Point</a></li>
-                                	<li><hr style="margin: 0"></li>
+                                    <li><a href="" > My Profile</a></li>
+                                    <li><a href="">My Point</a></li>
                                     <li>
                                         <a href="{{ route('logout') }}"
                                             onclick="event.preventDefault();
@@ -43,13 +54,9 @@
                                     </li>
                                 </ul>
                             </li>
-             @else
-                 <li><a href="{{ url('/login') }}">Login</a></li>
-                 <li><a href="{{ url('/register') }}">Register</a></li>
-             @endif
-        @endif
-     </ul>
-	 </div>
-
-</nav>
-<script src="{{ asset('js/app.js') }}"></script>
+                        @endif
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <script src="{{ asset('js/app.js') }}"></script>
