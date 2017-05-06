ed575cb858b9d24121724a5495aa7ee8a946d6de
Kamonwut <kamonwut.c@ku.th>
2017-05-06 16:45:06 +0700

    Add seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,16 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        App\User::create([
+            'id' => '1',
+            'fname' => 'admin',
+            'lname' => 'admin',
+            'email' => 'admin@example.com',
+            'password' => '$2y$10$B6R/MGVkF9QM.oxGlaoO8.1GxMEiua3JILJFfo.WzYFfr.KnyqbPW',
+            'point' => '100',
+            'pathimg' => 'https://wog.ua/images/user_icon.png',
+            'role' => 'admin'
+        ]);
     }
 }
