22298be4e7ba902504aab5fb2d38ef8207140768
Unknown <waritthorn.v@ku.th>
2017-05-07 19:42:35 +0700

    Auto stash before merge of "master" and "origin/master"

--- app/Http/Controllers/updatepromotionController.php
+++ app/Http/Controllers/updatepromotionController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\updatepromotion;
+use DB;
 
 class updatepromotionController extends Controller
 {
     function update(request $request){
       DB::table('promotion')
-            ->where('id', $request->input('promo'))
+            ->where('no', $request->input('promo'))
             ->update(array('path' => $request->input('promo-path')));
       return view('admin');
     }
