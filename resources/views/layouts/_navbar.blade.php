653a9850feaec98904a3a52ff4b66ace2c6d5d7b
Unknown <waritthorn.v@ku.th>
2017-05-08 22:35:45 +0700

    Change icon

--- resources/views/layouts/_navbar.blade.php
+++ resources/views/layouts/_navbar.blade.php
@@ -13,7 +13,7 @@
                     <!-- Branding Image -->
                     <div class="navbar-header">
                     <a class="navbar-brand" href="{{ url('/') }}">
-                        <img alt="Brand" id="home-icon" src="http://www.novelsol.com/wp-content/uploads/2014/12/flaticon-grad.png">
+                        <img alt="Brand" id="home-icon" src="http://icons.iconarchive.com/icons/icons8/windows-8/512/Music-Piano-icon.png">
                     </a>
                     </div>
                 </div>
